f5437207bdeb4471f63efdc9f2a74a306cc66090
Raccoon and friends <amauri00007@gmail.com>
2025-08-29 08:46:04 -0300

    Add files via upload

--- cs2panel/api.php
+++ cs2panel/api.php
@@ -12,7 +12,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Verifica a conexão
 if ($conn->connect_error) {
-    die(json_encode(["error" => "Falha na conexão: " . $conn->connect_error]));
+    die(json_encode(["error" => "Falha na conexão: " . $conn->connect_error], JSON_UNESCAPED_UNICODE));
 }
 
 // Obtém os parâmetros da URL
@@ -45,23 +45,43 @@ if ($type === 'admins') {
     // Consulta para obter os dados paginados e ordenados
     $sql = "SELECT steamid, reason, unbanned, timestamp FROM bans ORDER BY timestamp DESC LIMIT $itemsPerPage OFFSET $offset";
     $result = $conn->query($sql);
+
+} elseif ($type === 'ip_bans') {
+    // Consulta para contar o total de bans por IP
+    $countSql = "SELECT COUNT(*) AS total_count FROM ip_bans";
+    $countResult = $conn->query($countSql);
+    $totalCount = $countResult->fetch_assoc()['total_count'];
+
+    // Consulta para obter os dados paginados e ordenados
+    $sql = "SELECT ip_address, reason, unbanned, timestamp FROM ip_bans ORDER BY timestamp DESC LIMIT $itemsPerPage OFFSET $offset";
+    $result = $conn->query($sql);
+
+} elseif ($type === 'mutes') {
+    // Lógica para a nova tabela de mutados
+    $countSql = "SELECT COUNT(*) AS total_count FROM mutes";
+    $countResult = $conn->query($countSql);
+    $totalCount = $countResult->fetch_assoc()['total_count'];
     
+    $sql = "SELECT steamid, reason, unmuted, timestamp FROM mutes ORDER BY timestamp DESC LIMIT $itemsPerPage OFFSET $offset";
+    $result = $conn->query($sql);
 } else {
-    die(json_encode(["error" => "Tipo de dados inválido."]));
+    die(json_encode(["error" => "Tipo de dados inválido."], JSON_UNESCAPED_UNICODE));
 }
 
 $data = [];
 if ($result && $result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        // Converte o booleano de 'unbanned' para o tipo correto para o JavaScript
+        // Converte o booleano de 'unbanned' e 'unmuted' para o tipo correto para o JavaScript
         if (isset($row['unbanned'])) {
             $row['unbanned'] = (bool)$row['unbanned'];
+        }
+        if (isset($row['unmuted'])) {
+            $row['unmuted'] = (bool)$row['unmuted'];
         }
         $data[] = $row;
     }
 }
 
-echo json_encode(["data" => $data, "total_count" => $totalCount]);
-
+echo json_encode(["data" => $data, "total_count" => $totalCount], JSON_UNESCAPED_UNICODE);
 $conn->close();
 ?>
